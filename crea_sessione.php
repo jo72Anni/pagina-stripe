639f915816807167c24e40e9e99a61d3b28ac970
jo72Anni <perdifumo72@libero.it>
2025-05-04 09:54:52 +0200

    Update crea_sessione.php

--- crea_sessione.php
+++ crea_sessione.php
@@ -1,8 +1,11 @@
 <?php
 require 'vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
+// Controlla se il file .env esiste prima di caricarlo
+if (file_exists(__DIR__ . '/.env')) {
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
+}
 
 \Stripe\Stripe::setApiKey($_ENV['STRIPE_SECRET_KEY']);
 
